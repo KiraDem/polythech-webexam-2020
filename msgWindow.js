b1f64b61a19695566f18722599a5adf5c284ebb0
KiraDem <76489881+KiraDem@users.noreply.github.com>
2020-12-22 19:55:40 +0300

    Delete msgWindow.js

--- msgWindow.js
+++
@@ -1,8 +0,0 @@
-function showMsg (msg, category='success'){
-    let msgContainer = document.querySelector('.messages');
-    msgElement = document.createElement('div');
-    msgElement.innerHTML = msg;
-    msgElement.classList.add('msg');
-    msgElement.classList.add(Category);
-    msgContainer.append(msgElement);
-}
